e9726965717b445fe0b8148713035cb16e259230
Cory <oneillco117@gmail.com>
2021-11-09 16:12:18 -0500

    committed

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,19 @@
-# Your code goes here!
+require 'pry'
+
+class Anagram
+
+
+  def initialize(listen)
+    @listen = listen
+  end
+
+  def match(words)
+    words.select do |word|
+      is_listen?(word) 
+    end
+  end
+ 
+  def is_listen?(word)
+    word.chars.sort == @listen.chars.sort
+  end
+end
